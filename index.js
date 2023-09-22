d41566af5c53e7e657ba6069bd7218f15963f7c7
ravikant706 <ravikant706@gmail.com>
2023-09-22 12:55:05 +0530

    check button take click commond and show 'I got click'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.querySelector('button').addEventListener('click', handleClick);
-function handleClick() {
-    alert("I got clicked");
+for(i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {alert('hello')});
+
 }
