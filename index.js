67bf5e8638b7cb03e194d3a334efa71ec045f7cf
ravikant706 <ravikant706@gmail.com>
2023-09-23 12:09:17 +0530

    when click on button then listen all sound of drums

--- index.js
+++ index.js
@@ -3,5 +3,34 @@ document.querySelector(".w").addEventListener("click", function () {
     tom1.play();                          
 })
 
+document.querySelector(".a").addEventListener("click", function () {
+    var tom1 = new Audio('sounds/tom-2.mp3');
+    tom1.play();                          
+})
+
+document.querySelector(".s").addEventListener("click", function () {
+    var tom1 = new Audio('sounds/tom-3.mp3');
+    tom1.play();                          
+})
+
+document.querySelector(".d").addEventListener("click", function () {
+    var tom1 = new Audio('sounds/tom-4.mp3');
+    tom1.play();                          
+})
+
+document.querySelector(".j").addEventListener("click", function () {
+    var tom1 = new Audio('sounds/snare.mp3');
+    tom1.play();                          
+})
+
+document.querySelector(".k").addEventListener("click", function () {
+    var tom1 = new Audio('sounds/crash.mp3');
+    tom1.play();                          
+})
+
+document.querySelector(".l").addEventListener("click", function () {
+    var tom1 = new Audio('sounds/kick.mp3');
+    tom1.play();                          
+})
 
 
