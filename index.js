220d0e58d9a653c8dac5180d9503ee305759d9c3
ravikant706 <ravikant706@gmail.com>
2023-09-23 11:07:02 +0530

    tom1 sound add

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-for(i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    
-    document.querySelectorAll('.drum')[i].addEventListener('click', function () {alert('hello')});
+
+for( var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.InnerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            
+               
+            default: console.log(buttonInnerHTML);
+                break;
+        }
+
+    }); 
 
 }
+
+
+
